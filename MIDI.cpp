375294c1c1575f7e8a17d159ab2172216cd649fa
pee-que-elle <pql@cock.li>
2018-05-25 00:10:23 +0200

    Mode info printing from main() to MIDI class method

--- MIDI.cpp
+++ MIDI.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "MIDI.h"
 
+#include <iomanip>
+#include "HeaderChunk.h"
+#include "TrackChunk.h"
+
 MIDI::MIDI()
 {
 }
@@ -15,4 +19,28 @@ void MIDI::AddChunk(MIDIChunk *chunk)
 	Helpers::AssertError(chunk->type == HEADER && this->chunks_parsed != 0, "Header chunk at faulty index.");
 	this->chunks.push_back(chunk);
 	this->chunks_parsed++;
+}
+
+void MIDI::PrintInfo()
+{
+	for (int i = 0; i < this->chunks.size(); ++i)
+	{
+		MIDIChunk*& c = this->chunks[i];
+
+		std::cout << "Chunk #" << i << ": " << std::endl;
+		std::cout << "Type: " << (c->type == HEADER ? "Header" : "Track") << std::endl << std::endl;
+		std::cout << std::setfill('-') << std::setw(100) << "" << std::endl;
+		switch (c->type)
+		{
+		case HEADER:
+			std::cout << dynamic_cast<HeaderChunk*>(c)->Info();
+			break;
+		case TRACK:
+			std::cout << dynamic_cast<TrackChunk*>(c)->Info();
+			break;
+		}
+		std::cout << std::setfill('-') << std::setw(100) << "" << std::endl;
+
+		std::cout << std::endl << std::endl << std::endl;
+	}
 }
